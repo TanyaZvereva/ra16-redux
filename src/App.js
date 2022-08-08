9aa06c7b35960650f6c185a5f9f7988bf364b296
TanyaZvereva <darkmarketgirl3@yandex.ru>
2022-08-08 17:33:06 +0300

    commit 08.08

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import { List } from './features/list/List';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Counter />
+        <List/>
       </header>
     </div>
   );
--- src/features/list/List.js
+++ src/features/list/List.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   add,
@@ -12,32 +12,38 @@ export function List() {
   const dispatch = useDispatch();
   const [isEdit, setIsEdit] = useState(false);
   const [form, setForm] = useState({name:'', price: ''})
-  const handleAdd = () => {
-    dispatch(add(form))
-    setForm({name:'', price: ''})
-  }
-  const handleEdit = () => {
-    dispatch(edit(form))
+  const editKey = useRef(null)
+  const handleCancel = () => {
     setForm({name:'', price: ''})
     setIsEdit(false)
   }
+  const handleAdd = () => {
+    dispatch(add(form))
+    handleCancel()
+  }
+  const handleEdit = () => {
+    dispatch(edit({item:form, key:editKey.current}))
+    handleCancel()
+  }
+ 
   return (
     <div>
       <div>
         <input value={form.name} onChange={(event)=>setForm(prev=>({...prev,name:event.target.value}))}/>
         <input value={form.price} onChange={(event)=>setForm(prev=>({...prev,price:event.target.value}))}/>
-        <button>Save</button>
-        {isEdit && <button>Cancel</button>}
+        <button onClick={isEdit ? handleEdit : handleAdd}>Save</button>
+        {isEdit && <button onClick={handleCancel}>Cancel</button>}
       </div>
        {list.map((item, key) => {
-         return <div>
+         return <div key={item.name + key}>
             <span>{item.name}</span>
             <span>{item.price}</span>
             <button onClick={() => {
                 setIsEdit(true)
                 setForm(item)
+                editKey.current = key
             }}>Edit</button>
-            <button onClick={() => dispatch(remove(key))}>Remove</button>
+            <button onClick={() => isEdit ? (handleCancel(), dispatch(remove(key))):dispatch(remove(key))}>Remove</button>
          </div>
        })}
     </div>
